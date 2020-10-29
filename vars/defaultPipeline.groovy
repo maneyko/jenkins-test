76d2688575864d2a14b308b8a78a62a5a3af8c91
Peter Maneykowski <pmaneyko@gmail.com>
2020-10-29 15:16:22 -0500

    add project name

--- vars/defaultPipeline.groovy
+++ vars/defaultPipeline.groovy
@@ -1,10 +1,11 @@
-def call() {
+def call(projectName = "none") {
     pipeline {
         agent any
         stages {
             stage('Build') {
                 steps {
                     sh 'echo "Hello World"'
+                    sh "echo 'The project name is ${projectName}'"
                     sh '''
                         echo "Multiline shell steps works too"
                         ls -lah
