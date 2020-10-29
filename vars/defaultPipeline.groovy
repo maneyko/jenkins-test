cf1700fe7bcd60f933c54d0890d47a05bed5d087
Peter Maneykowski <pmaneyko@gmail.com>
2020-10-29 15:08:44 -0500

    add defaultPipeline

---
+++ vars/defaultPipeline.groovy
@@ -0,0 +1,16 @@
+def defaultPipeline() {
+    pipeline {
+        agent any
+        stages {
+            stage('Build') {
+                steps {
+                    sh 'echo "Hello World"'
+                    sh '''
+                        echo "Multiline shell steps works too"
+                        ls -lah
+                    '''
+                }
+            }
+        }
+    }
+}
