1b4f53826d2c2f3793878668e7e54927f69720ad
Peter Maneykowski <pmaneyko@gmail.com>
2020-10-29 15:38:08 -0500

    add more logic

--- vars/defaultPipeline.groovy
+++ vars/defaultPipeline.groovy
@@ -1,8 +1,9 @@
-def call(projectName = "none") {
+def call(projectName = "none", boolVar = true) {
     pipeline {
         agent any
         stages {
             stage('Build') {
+                when { boolVar }
                 steps {
                     sh 'echo "Hello World"'
                     sh "echo 'The project name is ${projectName}'"
@@ -11,8 +12,23 @@ def call(projectName = "none") {
                         echo "Multiline shell steps works too"
                         ls -lah
                     '''
+                    func()
+                }
+                when { ! boolVar }
+                steps {
+                    sh 'echo "boolVar" is falsey!'
+                    func()
+                }
+            }
+            post {
+                always {
+                    sh 'echo post'
                 }
             }
         }
     }
+}
+
+def func() {
+    sh 'echo "this is func()"'
 }
