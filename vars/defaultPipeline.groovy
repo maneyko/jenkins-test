4f5d037c0492aeb637fac17395dd82ed8665c83f
Peter Maneykowski <pmaneyko@gmail.com>
2020-10-29 22:58:33 -0500

    use when again

--- vars/defaultPipeline.groovy
+++ vars/defaultPipeline.groovy
@@ -15,8 +15,35 @@ def call(projectName = "none", boolVar = false) {
                 }
                 stages {
                     stage('Tests') {
+                        when { expression { projectName ==~ /branch/ } }
                         steps {
                             sh 'echo "Hello World"'
+                            func()
+                            sh "echo 'The project name is ${projectName}'"
+                            sh '''
+                              if test -n "$ENVVAR5"; then
+                                echo 1
+                              fi
+                            '''
+                            sh '''
+                              echo var1 $ENVVAR1
+                              echo var2 $ENVVAR2
+                              echo var3 $ENVVAR3
+                              echo var4 $ENVVAR4
+                            '''
+                            sampleMethod(projectName)
+                            sh '''
+                                echo "Multiline shell steps works too"
+                                ls -lah
+                            '''
+                            sh """
+                                if test -n '${boolVar ? "true" : ""}'; then
+                                  echo 'was true'
+                                else
+                                  echo 'was false'
+                                fi
+                            """
+                            sh 'echo hello!'
                         }
                         post {
                             always {
