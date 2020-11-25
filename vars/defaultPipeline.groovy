800901673972f0447c5e014277236e04f055d66a
Peter Maneykowski <pmaneyko@gmail.com>
2020-11-24 23:40:14 -0600

    send status

--- vars/defaultPipeline.groovy
+++ vars/defaultPipeline.groovy
@@ -77,6 +77,7 @@ def call(projectName = "none", boolVar = false) {
                                   parameters: [
                                     string(name: "repo_name", value: "jenkins-test"),
                                     string(name: "git_sha",   value: "$GIT_SHA")
+                                    string(name: "status",    value: currentBuild.currentResult)
                                   ],
                                   propagate: true,
                                   wait: true
